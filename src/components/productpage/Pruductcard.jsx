e70fef20d3d18fcc6c8ab993c0854442b2c18297
ehabatef205 <ehabmahmoud205@gmail.com>
2023-08-03 11:17:26 +0300

    remove build

--- src/components/productpage/Pruductcard.jsx
+++ src/components/productpage/Pruductcard.jsx
@@ -4,12 +4,12 @@ import React, { useState, useEffect, useContext } from "react";
 import "./slider.css";
 import Carousel from "react-bootstrap/Carousel";
 import { Card } from "react-bootstrap";
- import { useNavigate } from "react-router-dom";
- import { CartContext } from "../shoppingcart/shppingcartcontext";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../shoppingcart/shppingcartcontext";
 
- 
+
 export function CardsSlider() {
-  
+
   const { addToCart } = useContext(CartContext);
   const { addToWishlist } = useContext(CartContext);
 
@@ -18,12 +18,12 @@ export function CardsSlider() {
 
   const handleImageClick = (product) => {
     setSelectedProduct(product);
-    navigate(`/Cart`);
-    // navigate(`/Viewproduct/${product.id}`);
+    //navigate(`/Cart`);
+    navigate(`/Viewproduct/${product.id}`);
   };
 
 
-  
+
 
   return (
     <div className="containe d-flex  flex-wrap mx-1 ">
@@ -31,66 +31,66 @@ export function CardsSlider() {
         <div
           className=" my-1"
           key={product.id}
-          
+
         >
           <div className="carousel-wrapper"
 
-           >
-         
+          >
+
             <Carousel
-            
-            controls={false} 
-            onClick={() => handleImageClick(product)} 
-             >
+
+              controls={false}
+              onClick={() => handleImageClick(product)}
+            >
               {product.images.map((image, index) => (
                 <Carousel.Item key={index}>
-                  <img        
+                  <img
                     className="d-block w-100 "
-                    style={{height:"265px", objectFit: "contained" }}
+                    style={{ height: "265px", objectFit: "contained" }}
                     src={image.src}
                     alt={image.alt}
                   />
-                  
+
                 </Carousel.Item>
               ))}
-             
+
             </Carousel>
-           {/* <Nave></Nave> */}
-           <div className="caption position-absolute bottom-0 w-100  " >
-           {/* <button }> */}
+            {/* <Nave></Nave> */}
+            <div className="caption position-absolute bottom-0 w-100  " >
+              {/* <button }> */}
 
-<i className="bi bi-cart text-light mx-5" onClick={() => addToCart(product.id) }  role="button "style={{fontSize:"1.8rem",fontWeight:"bold", cursor: "pointer"}}></i>
+              <i className="bi bi-cart text-light mx-5" onClick={() => addToCart(product.id)} role="button " style={{ fontSize: "1.8rem", fontWeight: "bold", cursor: "pointer" }}></i>
 
 
-{/* <button  onClick={() => addToWishlist(product.id)} > */}
-<i   onClick={() => addToWishlist(product.id)}  role="button "className="bi bi-heart mx-5  text-light"style={{fontSize:"1.8rem",fontWeight:"bold", cursor: "pointer"}}></i>
+              {/* <button  onClick={() => addToWishlist(product.id)} > */}
+              <i onClick={() => addToWishlist(product.id)} role="button " className="bi bi-heart mx-5  text-light" style={{ fontSize: "1.8rem", fontWeight: "bold", cursor: "pointer" }}></i>
 
-{/* </button> */}
-</div>
-         
-                  
+              {/* </button> */}
+            </div>
+
+
           </div>
-          
-          
+
+
           <div
             className="card-body my-2 d-flex"
             style={{ fontSize: "0.8rem", padding: "0px" }}
           >
 
             <div className=" mx-2  d-flex flex-column align-items-start">
-           
+
               <Card.Text className="mb-0 text-secondary">{product.productname}</Card.Text>
-              <Card.Text className="mb-0"><s className="mx-2 " style={{color:"red"}}> ${product.old}</s> ${product.price}</Card.Text>
-           
-              <Card.Text className="mb-0"> 
-  {Array.isArray(product.color) ? 
-    product.color.map((color, index) => (
-      <i key={index} className={`bi bi-circle-fill text-${color}`} />
-    )) : null
-  }
-</Card.Text>
-              <Card.Text > <p style={{color:"red"}}>Final Sale - No Exchanges or Returns</p></Card.Text>
-      
+              <Card.Text className="mb-0"><s className="mx-2 " style={{ color: "red" }}> ${product.old}</s> ${product.price}</Card.Text>
+
+              <Card.Text className="mb-0">
+                {Array.isArray(product.color) ?
+                  product.color.map((color, index) => (
+                    <i key={index} className={`bi bi-circle-fill text-${color}`} />
+                  )) : null
+                }
+              </Card.Text>
+              <Card.Text > <p style={{ color: "red" }}>Final Sale - No Exchanges or Returns</p></Card.Text>
+
             </div>
           </div>
         </div>
--- src/headers/MainNave.jsx
+++ src/headers/MainNave.jsx
@@ -4,24 +4,24 @@ import SubNav from './SubNav';
 function MainNave() {
   return (
     <div className='d-flex flex-wrap '>
-      
-<nav className=" navbar navbar-expand-lg bg-body-tertiary mainnave col-6 col-lg-12 ">
-  <div className="container-fluid  ">
-   <div className=' col-12 col-lg-4 d-flex justify-content-center'>
-    <a className="navbar-brand  col-12 d-flex justify-content-center " style={{display:"flex",objectFit:"contain"}} href="/">
- <img  className='logo w-25' style={{height:"110px",  imageRendering:" pixelated"}} src="logo.png"></img>
- </a>
- </div>
- <div className="col-12  col-lg-5 my-2" style={{height:"6vh"}}>
- <div  style={container}>
- <div className=" mx-3  my-2 ">
-              <i className=" mx-2 bi bi-search-heart" style={{fontSize:"1.5rem"}}></i>
-             
+
+      <nav className=" navbar navbar-expand-lg bg-body-tertiary mainnave col-6 col-lg-12 ">
+        <div className="container-fluid  ">
+          <div className=' col-12 col-lg-4 d-flex justify-content-center'>
+            <a className="navbar-brand  col-12 d-flex justify-content-center " style={{ display: "flex", objectFit: "contain" }} href="/">
+              <img className='logo w-25' style={{ height: "110px", imageRendering: " pixelated" }} src="logo.png"></img>
+            </a>
+          </div>
+          <div className="col-12  col-lg-5 my-2" style={{ height: "6vh" }}>
+            <div style={container}>
+              <div className=" mx-3  my-2 ">
+                <i className=" mx-2 bi bi-search-heart" style={{ fontSize: "1.5rem" }}></i>
+
               </div>
               <input
-              placeholder='Search'
+                placeholder='Search'
                 style={{
-                    height:"6vh", 
+                  height: "6vh",
                   color: "#000",
                   border: "none",
                   outline: "none",
@@ -31,27 +31,23 @@ function MainNave() {
                   width: "100%",
                 }}
               />
-              
-              
+
+
             </div></div>
-            <div className="col-12 col-lg-3">
+          <div className="col-12 col-lg-3">
 
-              {/* <i className=" mx-2 bi bi-person"role="button"   href="/Login" style={{fontSize:"2rem"}}></i> */}
-             <a href="/Login"> <i className="mx-2 bi bi-person text-black" role="button"  style={{fontSize:"2rem"}}></i>
-             </a>
-              <a href="/Wishlist"><i className=" mx-2 bi bi-heart text-black" role="button" style={{fontSize:"2rem"}}></i></a>
-              <a >
+            {/* <i className=" mx-2 bi bi-person"role="button"   href="/Login" style={{fontSize:"2rem"}}></i> */}
+            <a href="/Login"> <i className="mx-2 bi bi-person text-black" role="button" style={{ fontSize: "2rem" }}></i>
+            </a>
+            <a href="/Wishlist"><i className=" mx-2 bi bi-heart text-black" role="button" style={{ fontSize: "2rem" }}></i></a>
 
-              <i className=" mx-2 bi bi-cart " data-bs-toggle="offcanvas" href="#offcanvasExample"  role="button" aria-controls="offcanvasExample"  style={{fontSize:"2rem"}}></i>
 
-              </a>
-                
-            </div>
-  </div>
-  
-</nav>
-<div className="col-6 col-lg-12 d-flex justify-content-center small-screen-only">
-      <SubNav ></SubNav>
+          </div>
+        </div>
+
+      </nav>
+      <div className="col-6 col-lg-12 d-flex justify-content-center small-screen-only">
+        <SubNav ></SubNav>
       </div>
     </div>
   )
@@ -66,5 +62,12 @@ const container = {
   paddingRight: "3%",
   marginRight: "3%",
   marginLeft: "3%",
-  borderRadius:5 ,
+  borderRadius: 5,
 };
+
+/*
+/*<a >
+
+              <i className=" mx-2 bi bi-cart " data-bs-toggle="offcanvas" href="#offcanvasExample"  role="button" aria-controls="offcanvasExample"  style={{fontSize:"2rem"}}></i>
+
+              </a>*/
